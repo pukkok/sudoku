e679fae24296dd76c376c19a7fd8198e93a2e26e
Pukkok <krystalgggg@gmail.com>
2024-06-26 00:46:28 +0900

    난이도 선택기능 추가하기

--- public/js/options.js
+++ public/js/options.js
@@ -31,47 +31,118 @@ function initNumber (board) {
     for(let x=0; x<9; x++){
       for(let y=0; y<9; y++){
         const data = document.createElement('div')
-        data.className = `data ${y+1}-${x+1}`
+        data.className = `cell`
         const input = document.createElement('input')
-  
+        input.className = `${y+1}-${x+1}`
+
         if(x<3){
-          if(y<3){
-              data.classList.add('small-box1') 
-          }else if(y<6){
-              data.classList.add('small-box2')
-          }else{
-              data.classList.add('small-box3')
-          }
+            if(y<3){
+                input.classList.add('box1')
+            }else if(y<6){
+                input.classList.add('box2')
+            }else{
+                input.classList.add('box3')
+            }
         }else if(x<6){
-          if(y<3){
-              data.classList.add('small-box4') 
-          }else if(y<6){
-              data.classList.add('small-box5')
-          }else{
-              data.classList.add('small-box6')
-          }
+            if(y<3){
+                input.classList.add('box4')
+            }else if(y<6){
+                input.classList.add('box5')
+            }else{
+                input.classList.add('box6')
+            }
         }else{
-          if(y<3){
-              data.classList.add('small-box7') 
-          }else if(y<6){
-              data.classList.add('small-box8')
-          }else{
-              data.classList.add('small-box9')
-          }
+            if(y<3){
+                input.classList.add('box7')
+            }else if(y<6){
+                input.classList.add('box8')
+            }else{
+                input.classList.add('box9')
+            }
+        }
+
+        if(x===0 || x===3 || x===6){
+            data.classList.add('top')
+        }
+        if(x===8){
+            data.classList.add('bottom')
+        }
+        if(y===0 || y===3 || y=== 6){
+            data.classList.add('left')
+        }
+        if(y===8){
+            data.classList.add('right')
         }
   
         input.value = `${board[x][y]}`
+        if(input.value){
+            input.readOnly=true
+        }
+    
         data.append(input)
         sudokuBox.append(data)
       }
     }
     return sudokuBox
 }
-initNumber(blankBoard)
-  
+
+(function firstStart () {
+    initNumber(blankBoard)
+    startGame()
+})()
+
 btnBox.querySelector('button').addEventListener('click', ()=>{
     const newBoard = sudoku()
     const newBlankBoard = makeBlankBoard(newBoard, level.level1)
     sudokuBox.innerHTML =''
     initNumber(newBlankBoard)
+    startGame()
 })
+
+function startGame () {
+    const inputs = document.querySelectorAll('.cell input')
+    // 클릭했을때
+    const focusInput = (e) => {
+        inputs.forEach(input => {
+            
+            if( // 같은 라인, 같은 박스
+                input.classList[0].split('-')[0] === e.target.classList[0].split('-')[0] || 
+                input.classList[0].split('-')[1] === e.target.classList[0].split('-')[1] ||
+                input.classList[1] === e.target.classList[1]
+            ){
+                input.style.backgroundImage = 'linear-gradient(rgba(249, 95, 213, 0.2), rgba(249, 95, 213, 0.2))'
+            }else{
+                input.style.backgroundImage = ''
+            }
+    
+            if(input.value === e.target.value){
+                if(input.value){
+                    input.style.backgroundImage = 'linear-gradient(rgba(249, 95, 213, 0.6), rgba(249, 95, 213, 0.6))'
+                }else{
+                    e.target.style.backgroundImage = 'linear-gradient(rgba(249, 95, 213, 0.6), rgba(249, 95, 213, 0.6))'
+                }
+            }
+        })
+    }
+    
+    // 정답 입력할때
+    const answerCheck = (e) => {
+        if(e.target.value.length>1){
+            // 1글자만 입력되도록 변경
+            e.target.value = e.target.value.charAt(1)
+        }
+        const coords = e.target.classList[0].split('-')
+        const answer = answerBoard[coords[1]-1][coords[0]-1]
+        
+        if(+e.target.value === answer){
+            e.target.style.color = 'blue'
+        }else{
+            e.target.style.color = 'red'
+        }
+    }
+    
+    inputs.forEach(input => {
+        input.addEventListener('click', focusInput)
+        input.addEventListener('input', answerCheck)
+    })
+}
